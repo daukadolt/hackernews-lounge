e9dbdd30ab7ae8535c879d5959a453e01e44b8c2
daukadolt <daulet.amirkhanov.official@gmail.com>
2020-06-11 23:55:24 +0600

    Updates

---
+++ precache-manifest.df6100d4dc7abfd06bebfd98d8c61a28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e98dafbef0dc4598eaed7c97dc99247",
+    "url": "/hackernews-lounge/index.html"
+  },
+  {
+    "revision": "8c75ee30536c2163fb21",
+    "url": "/hackernews-lounge/static/css/main.4d947bc1.chunk.css"
+  },
+  {
+    "revision": "039a8d73dd0de0d87112",
+    "url": "/hackernews-lounge/static/js/2.6fe86a44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackernews-lounge/static/js/2.6fe86a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c75ee30536c2163fb21",
+    "url": "/hackernews-lounge/static/js/main.7b4a7690.chunk.js"
+  },
+  {
+    "revision": "0b42cbc0242f7501cf1f",
+    "url": "/hackernews-lounge/static/js/runtime-main.e9a88ede.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ab5f2fe0e6ba09239a4fd03fd8bf04e1.js"
+  "/hackernews-lounge/precache-manifest.df6100d4dc7abfd06bebfd98d8c61a28.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackernews-lounge/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
